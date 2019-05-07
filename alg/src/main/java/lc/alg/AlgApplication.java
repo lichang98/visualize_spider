f428ebdc4691405169cd79a61a736bea2820cda8
lichang98 <15365811995@163.com>
2019-05-07 14:30:42 +0800

    修改项目的端口为80

--- alg/src/main/java/lc/alg/AlgApplication.java
+++ alg/src/main/java/lc/alg/AlgApplication.java
@@ -2,14 +2,21 @@ package lc.alg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class AlgApplication {
+public class AlgApplication implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		factory.setPort(80);	
 	}
 
 }
